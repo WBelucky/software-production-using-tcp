d779d1adba32d0d86a40994b2c0280ad453a51e6
WBelucky <shintaro.kusabiraki@gmail.com>
2020-06-17 23:41:23 +0900

    wip

--- myhttp/ZakoServer.java
+++ myhttp/ZakoServer.java
@@ -18,13 +18,7 @@ public class ZakoServer {
   public void listenAndServe(final int port) {
     try (final var s = new ServerSocket(port)) {
       while (true) {
-        this.service.execute(() -> {
-          try {
-            this.process(s.accept());
-          } catch (IOException e) {
-            e.printStackTrace(System.err);
-          }
-        });
+        this.process(s);
       }
     } catch (Exception e) {
       e.printStackTrace(System.err);
@@ -45,41 +39,45 @@ public class ZakoServer {
     return this;
   }
 
-  private void process(final Socket socket) throws IOException {
-    try (final var in = socket.getInputStream(); final var out = socket.getOutputStream();) {
-      final var req = new HttpRequest(in);
+  private void process(final ServerSocket s) throws IOException {
+    final var socket = s.accept();
+    this.service.execute(() -> {
+      try (final var in = socket.getInputStream(); final var out = socket.getOutputStream();) {
+        final var req = new HttpRequest(in);
 
-      final var header = req.header;
-      final var method = header.method;
-      final var path = header.path;
-      // System.out.println(header.text);
+        final var header = req.header;
+        final var method = header.method;
+        final var path = header.path;
+        // System.out.println(header.text);
 
-      if ("GET".equals(method)) {
-        System.out.println("kita");
-        // setFileで指定したところをstaticファイルにする
-        final var file = new File("static/", header.path);
-        if (file.exists() && file.isFile()) {
-          new HttpResponse(out).sendFile(file);
-          System.out.println("aru");
+        if ("GET".equals(method)) {
+          System.out.println("kita");
+          // setFileで指定したところをstaticファイルにする
+          final var file = new File("static/", header.path);
+          if (file.exists() && file.isFile()) {
+            new HttpResponse(out).sendFile(file);
+            System.out.println("aru");
+            return;
+          }
+          System.out.println("nai");
+        }
+        final var task = this.routeTasks.stream().filter(t -> t.method.equals(method) && t.path.equals(path))
+            .findFirst();
+        if (task.isEmpty()) {
+          // TODO: Not Found
           return;
         }
-        System.out.println("nai");
-      }
-      final var task = this.routeTasks.stream().filter(t -> t.method.equals(method) && t.path.equals(path)).findFirst();
-      if (task.isEmpty()) {
-        // TODO: Not Found
-        return;
-      }
-      task.get().procedure.accept(new Context(req, new HttpResponse(out)));
+        task.get().procedure.accept(new Context(req, new HttpResponse(out)));
 
-    } catch (IOException e) {
-      throw new UncheckedIOException(e);
-    } finally {
-      try {
-        socket.close();
       } catch (IOException e) {
-        e.printStackTrace(System.err);
+        throw new UncheckedIOException(e);
+      } finally {
+        try {
+          socket.close();
+        } catch (IOException e) {
+          e.printStackTrace(System.err);
+        }
       }
-    }
+    });
   }
 }
